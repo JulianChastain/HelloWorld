d2a730552527e9cea5aa726ef93c98898dec1a93
mattfranchi27 <mwfranc@clemson.edu>
2019-10-19 14:12:09 -0400

    5

--- nextWord.py
+++ nextWord.py
@@ -30,10 +30,12 @@ def make_pairs(words):
         yield (words[i], words[i+1])
 
 # MAIN
+
+print("Training Files: Shakespearean (1)\n")
 try:
     userChoice = int(input("Enter your choice of training file: "))
 except TypeError:
-    print("Invalid input. Program exiting...")
+    print("Invalid input. Program exiting...\n")
     exit(1)
 
 filename = filenameList[userChoice-1]
@@ -41,12 +43,13 @@ filename = filenameList[userChoice-1]
 try:
     textFile = open("newTest.txt","r")
 except FileNotFoundError:
-    print('File not found. Program exiting...')
+    print('File not found. Program exiting...\n')
     exit(2)
 
 parsedList = buildList(textFile)
 pairs = make_pairs(parsedList)
 word_dict = {}
+
 for word_1, word_2 in pairs:
     if word_1 in word_dict.keys():
         word_dict[word_1].append(word_2)
@@ -54,11 +57,11 @@ for word_1, word_2 in pairs:
         word_dict[word_1] = [word_2]
 
 firstWord = ""
-print("Finished Processing")
-while firstWord != "done":
-    firstWord = input()
+print("Finished Processing\n")
+while firstWord != "q":
+    firstWord = input("\nEnter a word: ")
     if firstWord.lower() in word_dict:
         print(random.choice(word_dict[firstWord.lower()]))
     else:
-        print("Not a valid word")
-    print("\n")
+        print("Not a valid word\n")
+
