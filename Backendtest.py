efed69f61446187946dc460aa8a81cf523c99352
JulianChastain <julian.g.chastain@gmail.com>
2019-10-19 11:38:20 -0400

    most recent commit4

---
+++ Backendtest.py
@@ -0,0 +1,13 @@
+class nextWordHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write(nextWord(""))
+
+    def make_app():
+        return tornado.web.Application([
+            (r"/(.*)", web.StaticFileHandler, {"path": "/TextFile"}),
+        ])
+
+if __name__ == "__main__":
+    app = make_app()
+    app.listen(8888)
+    tornado.ioloop.IOLoop.current().start()
