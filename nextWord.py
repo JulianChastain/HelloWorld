85841518e371c871f2b7102f61be066e7575f612
JulianChastain <julian.g.chastain@gmail.com>
2019-10-19 10:56:04 -0400

    Not yet working, but making progress

--- nextWord.py
+++ nextWord.py
@@ -1,9 +1,9 @@
 
-#will take the previous word entered by the
-#Need to analyze the words given in the textfile (chain.py) and decide the probability
-#of what the next word will be
+# will take the previous word entered by the
+# Need to analyze the words given in the textfile (testData.txt) and decide the
+# probability of what the next word will be
 def nextWord(lastword):
-  return "Clemson"
+  return lastword + "Clemson"
 
 #from https://github.com/ashwinmj/word-prediction/blob/master/MarkovModel.ipynb
 import string
@@ -30,7 +30,11 @@ def list2probabilitydict(given_list):
         probability_dict[item] = probability_dict.get(item, 0) + 1
     for key, value in probability_dict.items():
         probability_dict[key] = value / given_list_length
-    return probability_dict
+    return
+
+initial_word = {}
+second_word = {}
+transitions = {}
 
 # Trains a Markov model based on the data in training_data_file
 def train_markov_model():
@@ -64,4 +68,15 @@ def train_markov_model():
 
     print('Training successful.')
 
-train_markov_model()
+def sample_word(dictionary):
+    p0 = np.random.random()
+    cumulative = 0
+    for key, value in dictionary.items():
+        cumulative += value
+        if p0 < cumulative:
+            return key
+    assert(False)
+
+
+firstWord = input()
+print(sample_word(second_word[firstWord]))
