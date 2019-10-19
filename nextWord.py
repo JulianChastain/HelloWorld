fc10dafc1cef20f1bb04ffbb023b1ef5ce391451
JulianChastain <julian.g.chastain@gmail.com>
2019-10-19 12:45:43 -0400

    Make a working dictionary from list
    Creates a dictionary where each subsequent character is listed the number of times it occurs

--- nextWord.py
+++ nextWord.py
@@ -6,3 +6,21 @@ def nextWord(lastword):
   return lastword + "Clemson"
 
 import numpy as py
+
+words = ["hello", "world", "test", "data", "hello", "us"]
+
+def make_pairs(words):
+    for i in range(len(words)-1):
+        yield (words[i], words[i+1])
+
+pairs = make_pairs(words)
+
+word_dict = {}
+
+for word_1, word_2 in pairs:
+    if word_1 in word_dict.keys():
+        word_dict[word_1].append(word_2)
+    else:
+        word_dict[word_1] = [word_2]
+
+print(word_dict)
